e27103cf8a848874e5cc9dc7e3cc89bee1843d7d
sraja5037 <122851039+sraja5037@users.noreply.github.com>
2023-01-18 09:56:36 +0530

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Alpha {
+    public void foo(String args) {
+    	System.out.println("Alpha foo");
+    }
+    public void bar(String a) {
+    	System.out.println("Alpha bar");
+    }
+}
